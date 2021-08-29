4589060890bb8f87fbd2d6b130b8e58a7671df73
DnRT <eugenio.cortes@alumnos.uv.cl>
2021-08-29 17:51:09 -0400

    feat(GitHub): Finishing testing functions

--- GitHub/index.js
+++ GitHub/index.js
@@ -1,11 +1,13 @@
 const {Octokit} = require('@octokit/rest');
 const configJson = require('./config.json');
+//Initialize client, needed for every action, it need a key in object format
 const client = new Octokit({
     auth:configJson.token
 });
 
 const user='DnRT';
 const repo='Allie-Testing-API';
+//Create an issue, only need a title
 function createIssue(titleIssue){
     client.rest.issues.create({
         owner:user,
@@ -13,24 +15,45 @@ function createIssue(titleIssue){
         title:titleIssue 
     });
 }
+//Close an issue, issue number needed
 function closeIssue(number){
-    client.rest.issues.lock({
+    client.rest.issues.update({
         owner:user,
         repo:repo,
-        issue_number:number
+        issue_number:number,
+        state:"closed"
     });
 }
-let issues=client.rest.issues.listForRepo({
-    owner:user,
-    repo:repo
-}).then((response)=>{
-    const titleA= "Creating issue test";
-    for(let x=0;x<response.data.length;x++){
-        if(response.data[x].title===titleA){
-            closeIssue(response.data[x].number);
-        }
-        else{
-            createIssue(titleA);
-        }
-    }
-});
+//Add a colaborator
+function addColaboratorToRepo(colaborator){
+    client.rest.repos.addCollaborator({
+        owner:user,
+        repo:repo,
+        username:colaborator
+    });
+}
+//Add colaborator with specific/all permissions
+function addColaboratorToRepo(colaborator,permissions){
+    client.rest.repos.addCollaborator({
+        owner:user,
+        repo:repo,
+        username:colaborator,
+        permission:permissions
+    });
+}
+//Create a project on a repo
+function createProject(titleP){
+    client.rest.projects.createForRepo({
+        owner:user,
+        repo:repo,
+        name:titleP
+    })
+}
+//Update project card
+function updateCard(id,newNote,arch){
+    client.rest.projects.updateCard({
+        card_id:id,
+        note:newNote,
+        archived:arch
+    })
+}
